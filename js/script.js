fce0cc6a6449b2f1ce29f06f25527b38d04592d9
Katie elizabeth French <845855192@bun2-206-16.local>
2018-05-22 11:49:06 +1200

    basic HTML complete, JS DOM Queries defined

---
+++ js/script.js
@@ -0,0 +1,8 @@
+(function() {
+    var body = document.querySelector("body");
+    var inputWrapper = document.getElementById("inputWrapper");
+    var submitButton = document.getElementById("inputSubmit");
+    var getUserInput = document.getElementById("userInput").value;
+
+
+}());
