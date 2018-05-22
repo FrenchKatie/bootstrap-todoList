0dc0d11cae2a786f2304b96bd39a277424c73599
Katie French <frenchkatie8@gmail.com>
2018-05-22 14:50:30 +1200

    User Input save to console is working!

--- js/script.js
+++ js/script.js
@@ -1,8 +1,54 @@
 (function() {
-    var body = document.querySelector("body");
-    var inputWrapper = document.getElementById("inputWrapper");
-    var submitButton = document.getElementById("inputSubmit");
-    var getUserInput = document.getElementById("userInput").value;
+    // var body = document.querySelector("body");
+    // var inputWrapper = document.getElementById("inputWrapper");
+    // var submitButton = document.getElementById("inputSubmit");
+    // // var getUserInput = document.getElementById("userInput");
+    // // console.log(getUserInput);
+    //
+    // function addListItem() {
+    //     console.log(getUserInput);
+    // }
+    // submitButton.addEventListener('click', addListItem , false);
+
+    		//Access input element in DOM/HTML
+    		var input = document.getElementById('numbers');
+
+    		// Basic jQuery click event for testing
+    		$('button').click(function(){
+    			console.dir(input.value);
+
+    			// Function calls
+    			// testFalsy(input.value);
+    			// turnStringToNumber(input.value);
+    			// guess(input.value);
+    		});
+
+    		// function testFalsy (val) {
+    		// 	console.log(val);
+    		// 	val = val.toString();
+    		// 	if (val === "22") {
+    		// 		console.log('works');
+    		// 	}
+    		// }
+        //
+    		// function turnStringToNumber (val) {
+    		// 	if (val !== "22") {
+    		// 		console.log('yes!');
+    		// 	} else {
+    		// 		console.log('no!');
+    		// 	}
+    		// }
+        //
+    		// function guess (val) {
+    		// 			if (val < "22") {
+    		// 					console.log('guess higher!');
+    		// 			}else if(val === "22"){
+    		// 					console.log("you guessed the right number!");
+    		// 			}else {
+    		// 					console.log('guess lower!');
+    		// 			}
+    		// }
+
 
 
 }());
