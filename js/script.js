81e2a2236b5c0a86df1e8c96e995d43869cb982e
Katie French <frenchkatie8@gmail.com>
2018-05-22 17:15:40 +1200

    got user input to come out as a list working!

--- js/script.js
+++ js/script.js
@@ -1,53 +1,23 @@
 (function() {
-    // var body = document.querySelector("body");
-    // var inputWrapper = document.getElementById("inputWrapper");
-    // var submitButton = document.getElementById("inputSubmit");
-    // // var getUserInput = document.getElementById("userInput");
-    // // console.log(getUserInput);
-    //
-    // function addListItem() {
-    //     console.log(getUserInput);
-    // }
-    // submitButton.addEventListener('click', addListItem , false);
 
-    		//Access input element in DOM/HTML
-    		var input = document.getElementById('numbers');
+    		var input = document.getElementById('userInput');
+        // var userInputText = input.value;
 
-    		// Basic jQuery click event for testing
     		$('button').click(function(){
-    			console.dir(input.value);
-
-    			// Function calls
-    			// testFalsy(input.value);
-    			// turnStringToNumber(input.value);
-    			// guess(input.value);
+              makeNewElement(input.value);
+    			    console.dir(input.value); //why does this not work when i use the variable "userInputText"
     		});
 
-    		// function testFalsy (val) {
-    		// 	console.log(val);
-    		// 	val = val.toString();
-    		// 	if (val === "22") {
-    		// 		console.log('works');
-    		// 	}
-    		// }
-        //
-    		// function turnStringToNumber (val) {
-    		// 	if (val !== "22") {
-    		// 		console.log('yes!');
-    		// 	} else {
-    		// 		console.log('no!');
-    		// 	}
-    		// }
-        //
-    		// function guess (val) {
-    		// 			if (val < "22") {
-    		// 					console.log('guess higher!');
-    		// 			}else if(val === "22"){
-    		// 					console.log("you guessed the right number!");
-    		// 			}else {
-    		// 					console.log('guess lower!');
-    		// 			}
-    		// }
+        function makeNewElement(elementText) {
+              customControlValidation1.insertAdjacentHTML("afterend", "<label class='custom-control-label' for='customControlValidation1'>" + elementText + "</label><br>");
+        }
+
+
+// *** Will need to change the jquery click event to vanilla JS when I get it working *** //
+        // function addListItem() {
+        //     console.log(getUserInput);
+        // }
+        // submitButton.addEventListener('click', addListItem , false);
 
 
 
