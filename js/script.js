afc1d5e9a20af8f735bf493512ebbf0360743957
Katie elizabeth French <845855192@bun2-206-16.local>
2018-05-23 11:36:44 +1200

    List and checkboxes are all working!

--- js/script.js
+++ js/script.js
@@ -1,38 +1,27 @@
 (function() {
 
     		var input = document.getElementById('userInput');
-        // var userInputText = input.value;
+        var counter = 0;
 
     		$('button').click(function(){
-              makeNewElement(input.value);
-    			    console.dir(input.value); //why does this not work when i use the variable "userInputText"
+              addListItem(input.value);
     		});
 
-        function makeNewElement(elementText) {
-              var counter = 0;
-              window.uniqueId = function(){
-                  return 'customControlValidation' + counter++
-              }
-              var newItem = "     <input type='checkbox' class='custom-control-input' id='";
+        function addListItem(elementText) {
+
+              var uniqueId = 'customControlValidation' + counter++;
+
+              var newItem = "     <div class='custom-control custom-checkbox mb-3'><input type='checkbox' class='custom-control-input' id='";
           				newItem +=  		uniqueId;
           				newItem += "'   required><label class='custom-control-label' for='";
           				newItem +=      uniqueId;
           				newItem += "'   >";
           				newItem += 			elementText;
-          				newItem += "    </label><br>";
-              // customControlValidation1.insertAdjacentHTML("afterend", "<label class='custom-control-label' for='customControlValidation1'>" + elementText + "</label><br>");
-              // listItemWrapper.insertAdjacentHTML("afterend", "<input type='checkbox' class='custom-control-input' id='" + elementText + "' required><label class='custom-control-label' for='" + elementText + "'>" + elementText + "</label><br>");
+          				newItem += "    </label></div>";
+
               listItemWrapper.insertAdjacentHTML("afterend", newItem);
 
-
         }  // ** makeNewElement function ENDS **
-
-
-        // *** Will need to change the jquery click event to vanilla JS when I get it working *** //
-        // function makeNewElement() {
-        //     console.log(getUserInput);
-        // }
-        // submitButton.addEventListener('click', makeNewElement , false);
 
 
 
