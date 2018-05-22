b356d14077869e178a552c7a94a82f0facd78183
Katie elizabeth French <845855192@bun2-206-16.local>
2018-05-23 09:29:11 +1200

    changed JS to use counter and newItem variable to put on multiple lines for code readability (not functional yet)

--- js/script.js
+++ js/script.js
@@ -9,16 +9,31 @@
     		});
 
         function makeNewElement(elementText) {
-              customControlValidation1.insertAdjacentHTML("afterend", "<label class='custom-control-label' for='customControlValidation1'>" + elementText + "</label><br>");
-        }
+              var counter = 0;
+              window.uniqueId = function(){
+                  return 'customControlValidation' + counter++
+              }
+              var newItem = "     <input type='checkbox' class='custom-control-input' id='";
+          				newItem +=  		uniqueId;
+          				newItem += "'   required><label class='custom-control-label' for='";
+          				newItem +=      uniqueId;
+          				newItem += "'   >";
+          				newItem += 			elementText;
+          				newItem += "    </label><br>";
+              // customControlValidation1.insertAdjacentHTML("afterend", "<label class='custom-control-label' for='customControlValidation1'>" + elementText + "</label><br>");
+              // listItemWrapper.insertAdjacentHTML("afterend", "<input type='checkbox' class='custom-control-input' id='" + elementText + "' required><label class='custom-control-label' for='" + elementText + "'>" + elementText + "</label><br>");
+              listItemWrapper.insertAdjacentHTML("afterend", newItem);
 
 
-// *** Will need to change the jquery click event to vanilla JS when I get it working *** //
-        // function addListItem() {
+        }  // ** makeNewElement function ENDS **
+
+
+        // *** Will need to change the jquery click event to vanilla JS when I get it working *** //
+        // function makeNewElement() {
         //     console.log(getUserInput);
         // }
-        // submitButton.addEventListener('click', addListItem , false);
+        // submitButton.addEventListener('click', makeNewElement , false);
 
 
 
-}());
+}());  // ** iife ENDS
